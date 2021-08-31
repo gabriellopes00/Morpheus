e0f744217cc90f34e5c764e0a10ce871a3332a2d
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-08-31 16:46:24 -0300

    feat(mailer): create send email feature

--- mailer/src/app/app.ts
+++ mailer/src/app/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import amqp from 'amqplib/callback_api'
+import nodemailer from 'nodemailer'
 
 const {
   RABBITMQ_PORT,
@@ -7,6 +8,11 @@ const {
   RABBITMQ_VHOST,
   RABBITMQ_HOST,
   RABBITMQ_PASS,
+
+  SMTP_USER,
+  SMTP_PASS,
+  SMTP_PORT,
+  SMTP_HOST,
 } = process.env
 
 const options: amqp.Options.Connect = {
@@ -38,9 +44,27 @@ amqp.connect(options, (err, conn) => {
 
     const queue = 'account_created'
 
-    ch.assertQueue(queue, { durable: false })
+    ch.assertQueue(queue, { durable: true })
     ch.prefetch(1)
-    ch.consume(queue, (msg) => console.log(msg.content.toString()), {
+    ch.consume(queue, async (msg) => {
+      const account: Account = JSON.parse(msg.content.toString())
+
+      const transporter = nodemailer.createTransport({
+        host: SMTP_HOST,
+        port: Number(SMTP_PORT),
+        auth: {
+          user: SMTP_USER,
+          pass: SMTP_PASS
+        }
+      })
+
+      await transporter.sendMail({
+        from: "gabriellopes@morpheus.io", 
+        to: account.email,
+        text: `Well Come to Morpheus, ${account.name}<${account.email}>! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quasi eveniet dolore id recusandae accusantium molestias soluta expedita labore quia porro sint atque ducimus, voluptas veniam laudantium quaerat quo libero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis veniam neque minus ipsum deleniti exercitationem maxime dolore est temporibus pariatur laborum nemo libero, modi, porro sunt. Maiores esse error doloremque.`
+      })
+
+    }, {
       noAck: true,
     })
   })
