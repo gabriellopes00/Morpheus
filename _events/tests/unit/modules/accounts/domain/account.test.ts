a85d6ac58f85d3bd22089c8b402d47a63d2a31e9
Gabriel Lopes <gabrielluislopes00@gmail.com>
2022-05-21 09:57:34 -0300

    feat: create account entity

---
+++ _events/tests/unit/modules/accounts/domain/account.test.ts
@@ -0,0 +1,21 @@
+import { Account } from '@/modules/accounts/domain/account'
+
+describe('Account Entity', () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern')
+    jest.setSystemTime(new Date(2022, 3, 1))
+  })
+
+  afterAll(() => jest.useRealTimers())
+
+  it('Should create an account successfully', async () => {
+    const account = new Account(
+      { referencedId: 'e05625f2-4eb2-4a3e-8c3b-a5bb1d82d1f5' },
+      '7e40f883-f108-43ba-81ac-8f75b81b5c0b'
+    )
+
+    expect(account.createdAt).toEqual(new Date())
+    expect(account.referencedId).toEqual('e05625f2-4eb2-4a3e-8c3b-a5bb1d82d1f5')
+    expect(account.id).toEqual('7e40f883-f108-43ba-81ac-8f75b81b5c0b')
+  })
+})
