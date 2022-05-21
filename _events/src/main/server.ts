b66ec25f6eebffd54da6711ac9a96c557bda24d6
Gabriel Lopes <gabrielluislopes00@gmail.com>
2022-05-21 12:46:01 -0300

    chore: setup initial server

--- _events/src/main/server.ts
+++ _events/src/main/server.ts
@@ -1,5 +1,24 @@
-import 'module-alias/register'
+import { UidGenerator } from '@/infra/crypto/uuid-generator'
+import { PostgresDataSource } from '@/infra/db/data-source'
+import { PgAccountRepository } from '@/infra/db/repositories/account-repository'
+import { MessageQueue } from '@/infra/message-queue/rabbitmq'
+import { CreateAccount } from '@/modules/accounts/usecases/create-account'
+import { DeleteAccount } from '@/modules/accounts/usecases/delete-account'
 import 'dotenv/config'
-import logger from '../config/logger'
+import 'module-alias/register'
+;(async () => {
+  try {
+    const repo = new PgAccountRepository(PostgresDataSource)
 
-logger.info(process.env.PORT)
+    const saveAccount = new CreateAccount(new UidGenerator(), repo)
+    const deleteAccount = new DeleteAccount(repo)
+
+    // start consuming message queue
+    const queue = new MessageQueue(saveAccount, deleteAccount)
+    await queue.connect()
+    await queue.consume()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+})()
